91dfa41ea804bc69860ec4473d12d46fc51e3dbe
Tokhy <elt0khy@protonmail.com>
2021-03-29 00:27:20 +0200

    changes

--- mazeGenerator.cpp
+++ mazeGenerator.cpp
@@ -5,12 +5,12 @@ using namespace std;
 #define tt(n) for (int i=0; i<n; i++)
 #define FOR(i,a,n) for (auto i=a; i!=n; i++)
 
-bool validLimits(int x, int y, int nx, int ny) {
+bool validLimits(int x, int y, int n) {
 	if(
 			x<0 ||
-			x>=nx ||
+			x>=n ||
 			y<0 ||
-			y>=nx 
+			y>=n 
 	  ) return false;
 	return true;
 
@@ -38,13 +38,43 @@ void gimmeMaze(){
 
 	int grid[20][20];
 
+	//	memset(grid,-1,1600);
+
 	memset(grid,-1,1600);
 
-	vector<pair<int,int>> V = winningPath(grid, 18, 1);
+	FOR(i,0,20) {
+		FOR(j,0,20){
+			rand()%2?grid[i-1][j]=0:grid[i][j+1] = 0;
+		}		
+	}
 
-		
-	tt(90) vector<pair<int,int>> tmp = winningPath(grid, rand()%19, rand()%19);
-	
+	FOR(i,6,13) {
+		FOR(j,6,13) {
+			grid[i][j] = 0;
+		}
+	}
+	tt(5) {
+		grid[7][7+i] = -1;
+		grid[11][7+i] = -1;
+		grid[7+i][7] = -1;
+		grid[7+i][11] = -1;
+	}
+	grid[9][7] = 0;
+	grid[9][11] = 0;
+	grid[7][9] = 0;
+	grid[11][9] = 0;
+
+	tt(20) {
+		grid[0][i] = -1;
+		grid[19][i] = -1;
+		grid[i][0] =-1;
+		grid[i][19]=-1;
+	}
+	/*
+	   vector<pair<int,int>> V = winningPath(grid, 18, 1);
+	   tt(25) vector<pair<int,int>> tmp = winningPath(grid, rand()%19, rand()%19);
+	   */
+
 
 	tt(20) {
 		FOR(j,0,20)
